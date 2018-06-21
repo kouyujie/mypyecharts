76a013262207f91fc427da35095518a5e751e490
U-PC201502041815\Administrator <kouyujie@qq.com>
2018-06-21 19:46:53 +0800

    auto refresh

--- test.py
+++ test.py
@@ -1,22 +1,41 @@
 # coding=UTF-8  
-#  flask echart  实验  
-from flask import Flask, render_template  
+#  flask echart
+from flask import Flask, render_template
+import xlrd
+
 app = Flask(__name__)  
  
 @app.route('/')  
 def app_0():  
-    return "<h1>EChart数据演示</h1>  <br> <a href='/one'>柱状图</a> <br> <a href='/two'>仪表盘</a>"  
- 
-@app.route('/one')  
+    return "<h1>快修数据演示</h1>  <br> <a href='/one'>柱状图</a> <br> <a href='/two'>仪表盘</a>"
+
+@app.route('/one')
 def app_1():  
-    from pyecharts import Bar  
-  
-    bar = Bar("一周任务量", "快修组",width=1920,height=1080,background_color='#000')  
-    bar.add("审核量", ["周一", "周二", "周三", "周四", "周五", "周六","周日"], [5, 20, 36, 10, 75, 90,28])  
-    bar.add("修改量", ["周一", "周二", "周三", "周四", "周五", "周六","周日"], [25, 10, 56, 70, 25, 40,88])  
-    ret_html = render_template('pyecharts.html',  
+    from pyecharts import Bar
+    book = xlrd.open_workbook(u"快修-产量、效率、质量模板.xlsx")
+    s = book.sheet_by_index(1)
+
+    bar = Bar("每日任务量", "快修组",width=1910,height=1080,background_color='#fff')
+    i = 1
+    xx = []
+    yy = []
+
+    xx1 = []
+    yy1 = []
+    while i <= 20:
+        #print s.cell_value( 3+i,0 ),s.cell_value( 3+i,1 )
+        xx.append( s.cell_value( 3+i,0 ) )
+        yy.append( s.cell_value( 3+i,1 ) )
+        xx1.append( s.cell_value( 3+i,0 ) )
+        yy1.append( s.cell_value( 3+i,4 ) )
+        i += 1
+
+    bar.add("审核量",xx,yy,mark_line=["average"], mark_point=["max", "min"],is_label_show = 1,label_pos = 'inside' )
+    bar.add("修改量",xx1,yy1,mark_line=["average"], mark_point=["max", "min"],is_label_show = 1,label_pos = 'inside' )
+
+    ret_html = render_template('pyecharts.html',
                            myechart=bar.render_embed(),  
-                               mytitle=u"数据演示",  
+                               mytitle=u"每日任务量数据",
                            host='/static',  
                            script_list=bar.get_js_dependencies())  
     return ret_html  
@@ -25,18 +44,19 @@ def app_1():
 def app_2():  
     from pyecharts import Gauge  
   
-    gauge = Gauge("快修完成情况","任务数",width=600, height=300)  
+    gauge = Gauge("快修完成情况","任务数",width=800, height=400)  
     gauge.add("审核", "", 86.66,scale_range=[0,100],angle_range=[180,0])  
   
-    gauge1 = Gauge("快修完成情况","任务数",width=600, height=300)  
+    gauge1 = Gauge("快修完成情况","任务数",width=800, height=400)  
     gauge1.add("修改", "", 88.99)  
     ret_html = render_template('pyecharts.html',  
                            myechart=gauge.render_embed()+gauge1.render_embed(),  
-                               mytitle=u"数据演示",  
+                               mytitle=u"快修数据演示",
                            host='/static',  
                            script_list=gauge.get_js_dependencies())  
     return ret_html  
-  
+
+
 if __name__ == '__main__':  
   
-    app.run(host='0.0.0.0',port=5018)  
+    app.run(host='0.0.0.0',port=5018,debug=True)
